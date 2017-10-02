c0227ea55843bc33508e8d77642302ad1addd580
ArgunovAlexander <30368617+ArgunovAlexander@users.noreply.github.com>
2017-10-02 22:18:14 +0300

    Update chap13_1.java
    JTextArea Example from Head First Java p415

--- chap13_1.java
+++ chap13_1.java
@@ -1,26 +1,39 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Panel1 {
+public class TextArea1 implements ActionListener {
 
+  JTextArea text;
+  
   public static void main (String [] args) {
-    Panel1 gui=new Panel1();
+    TextArea1 gui=new TextArea1();
     gui.go();
-  }
-public void go() {
+  }//close main()
+
+  public void go() {
   JFrame frame=new JFrame();
   JPanel panel=new JPanel();
-  panel.setBackground(Color.darkGray);
+  JButton button=new JButton("Just click it");
+  button.addActionListener(this);
+  text=new JTextArea(10,20);
+  text.setLineWrap(true);
   
-  JButton button=new JButton("shock me");
-  JButton buttonTwo=new JButton("bliss");
-  JButton buttonThree=new JButton("huh?");
-  panel.add(button);
-  panel.add(buttonTwo);
-  panel.add(buttonThree);
+  JScrollPane scroller=new JScrollPane(text);
+  scroller.setVerticalScrollBarPolicy(ScrollPaneConsists.VERTICAL_SCROLLBAR_ALWAYS);
+  scroller.setHorizontalScrollBarPolicy(ScrollPaneConsists.HORIZONTAL_SCROLLBAR_NEVER);
   
-  frame.getContentPane().add(BorderLayout.EAST,panel);
-  frame.setSize(250,200);
+  panel.add(scroller);
+   
+  frame.getContentPane().add(BorderLayout.CENTER,panel);
+  frame.getContentPane().add(BorderLayout.SOUTH,button);
+  
+  frame.setSize(350,300);
   frame.setVisible(true);
 }//close go()
-}//close Panel1
+  
+public void actionPerformed(ActionEvent ev) {
+  text.append("button clicked \n ");
+}
+}//close TextArea1
+
